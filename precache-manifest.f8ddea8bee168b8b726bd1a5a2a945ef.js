c01307f009fc0966329b58ad40116717331b7f8a
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-07-22 03:29:52 +0430

    CV Website

---
+++ precache-manifest.f8ddea8bee168b8b726bd1a5a2a945ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ebbd8e19fbfb1ef42ad3172782bdbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e46da62e027ea88a94a",
+    "url": "/static/css/main.7b2332f6.chunk.css"
+  },
+  {
+    "revision": "6e3ea8c9026b85db749e",
+    "url": "/static/js/2.43711576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43711576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e46da62e027ea88a94a",
+    "url": "/static/js/main.b1302e4d.chunk.js"
+  },
+  {
+    "revision": "e1f4633d0b250b75e305",
+    "url": "/static/js/runtime-main.2cb59220.js"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "c54765236fe5cfee08032ade67758bcc",
+    "url": "/static/media/m2.c5476523.png"
+  }
+]);
